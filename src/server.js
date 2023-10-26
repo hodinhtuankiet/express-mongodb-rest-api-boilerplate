bbe80ba16f78a674afa48c28c1aa6f34ef74b4d6
Tuan Kiet <kiethdt.22ite@vku.udn.vn>
2023-10-26 17:38:17 +0700

    Init code base

--- src/server.js
+++ src/server.js
@@ -1,12 +1,33 @@
-import express from 'express'
-const app = express();
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
 
-const hostname = 'localhost';
-const port = 8017; 
-app.get('/', function (req, res) {
-    res.send('<h1>Welcome!</h1>');
+import express from 'express'
+import { mapOrder } from '~/utils/sorts.js'
+
+const app = express()
+
+const hostname = 'localhost'
+const port = 8017
+
+app.get('/', (req, res) => {
+  // Test Absolute import mapOrder
+  
+  console.log(mapOrder(
+    [{ id: 'id-1', name: 'One' },
+      { id: 'id-2', name: 'Two' },
+      { id: 'id-3', name: 'Three' },
+      { id: 'id-4', name: 'Four' },
+      { id: 'id-5', name: 'Five' }],
+    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+    'id'
+  ))
+  res.end('<h1>Hello World!</h1><hr>')
 })
 
-app.listen(port,hostname,()=>{
-    console.log(`hello tuan kiet dev,I'am running server on port http://${hostname}:${port}`);
+app.listen(port, hostname, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Hello Tuan Kiet Quan Dev, I am running at ${ hostname }:${ port }/`)
 })
