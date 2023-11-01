2d2ec9faa10db2dedeb21cd4bdf18242a1de3fb6
Tuan Kiet <kiethdt.22ite@vku.udn.vn>
2023-11-02 00:16:50 +0700

    [#4] connect mongodb

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,30 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+const { MongoClient, ServerApiVersion } = require("mongodb");
+import { env } from '~/config/environment'
+
+// init variable null (not connected yet) 
+let trelloDatabaseInstance = null
+
+// init object mongoClientInstance to connect to mongodb server
+const mongoClientInstance = new MongoClient( env.MONGODB_URL,{
+    // serverApi have 5.0 version higher 
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        // when u use version 7 so call methods of version 6 then deprecationErrors will announce errors 
+        deprecationErrors: true,
+    }
+    }
+)
+export const CONNECT_DB = async () =>{
+    // connect will be availbe when new MongoClient
+    // Call connect to MongoDB Atlas with URL declare in mongoClientInstance
+    await mongoClientInstance.connect()
+    // When finished connect , assign name db 
+    trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
+}
+// export when connect success to use many different places
+export const GET_DB = () =>{
+    if(!trelloDatabaseInstance) throw new Error('Must connect to database first')
+    // if available
+    return trelloDatabaseInstance;
+}
--- src/server.js
+++ src/server.js
@@ -1,33 +1,23 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
-
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import { env } from '~/config/environment'
+import { GET_DB , CONNECT_DB } from '~/config/mongodb'
 
-const app = express()
+const START_SERVER = ()=>{
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
-
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
+  app.get('/', (req, res) => {
+    res.end('<h1>Hello World!</h1><hr>')
+  })
   
-  console.log(mapOrder(
-    [{ id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' }],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
-
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Tuan Kiet Quan Dev, I am ádasdrunning at ${ hostname }:${ port }/`)
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Hello ${env.AUTHOR} Dev, I am running at ${ env.APP_HOST }:${ env.APP_PORT }/`)
+  })
+}
+GET_DB()
+.then(()=> console.log('Connected to MongoDB Clound Atlas'))
+.then(()=> START_SERVER())
+.catch(error => {
+  console.log(error);
+  process.exit(0)
 })
