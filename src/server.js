807f207a3e5b231aa80b6be0aa4b18f42c8fd396
Tuan Kiet <kiethdt.22ite@vku.udn.vn>
2023-11-03 22:24:57 +0700

    [#4 optimize connect mongo]

--- src/server.js
+++ src/server.js
@@ -5,7 +5,10 @@ import { GET_DB , CONNECT_DB } from '~/config/mongodb'
 const START_SERVER = ()=>{
   const app = express()
 
-  app.get('/', (req, res) => {
+  app.get('/', async (req, res) => {
+
+    console.log(await GET_DB().listCollections().toArray());
+
     res.end('<h1>Hello World!</h1><hr>')
   })
   
@@ -14,10 +17,22 @@ const START_SERVER = ()=>{
     console.log(`Hello ${env.AUTHOR} Dev, I am running at ${ env.APP_HOST }:${ env.APP_PORT }/`)
   })
 }
-GET_DB()
-.then(()=> console.log('Connected to MongoDB Clound Atlas'))
-.then(()=> START_SERVER())
-.catch(error => {
-  console.log(error);
-  process.exit(0)
-})
+
+(async ()=>{ 
+  try {
+    console.log('1.Connecting to MongoDB Clound Atlas...')
+    await CONNECT_DB()
+    console.log('2.Connected to MongoDB Clound Atlas')
+    START_SERVER()
+  } catch (error) {
+    console.log(error);
+    process.exit(0)
+  }
+})()
+// CONNECT_DB()
+// .then(()=> console.log('Connected to MongoDB Clound Atlas'))
+// .then(()=> START_SERVER())
+// .catch(error => {
+//   console.log(error);
+//   process.exit(0)
+// })
