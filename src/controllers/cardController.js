06873173046addbd5b49d35c5b3d1f4c8d6caa17
DTK-18 <120298180+hodinhtuankiet@users.noreply.github.com>
2024-03-08 00:48:09 +0700

    done

--- src/controllers/cardController.js
+++ src/controllers/cardController.js
@@ -1,6 +1,6 @@
 import { StatusCodes } from 'http-status-codes'
 import { cardService } from '~/services/cardService'
-
+import { cardModel } from '~/models/cardModel'
 const createNew = async (req, res, next ) => {
   try {
     console.log('anh o cardController: ', req.file)
@@ -12,8 +12,17 @@ const createNew = async (req, res, next ) => {
     next(error)
   }
 }
+const readDetail = async (cardId) => {
+  try {
+    const getDataCard = await cardModel.fineOneById(cardId)
+    // You might want to process or manipulate the data here before returning it
+    return getDataCard
+  } catch (error) {
+    throw new Error(error.message) // Re-throw the error with a more descriptive message
+  }
+}
 
 
 export const cardController = {
-  createNew
+  createNew, readDetail
 }
--- src/routes/v1/cardRoute.js
+++ src/routes/v1/cardRoute.js
@@ -27,4 +27,16 @@ Router.route('/')
       res.status(500).send('Internal Server Error')
     }
   }, cardValidation.createNew, cardController.createNew)
+Router.route('/:id')
+  .get(async (req, res) => {
+    const cardId = req.params.id
+    console.log(cardId)
+    try {
+      const cardData = await cardController.readDetail(cardId)
+      res.json(cardData) // Assuming you want to send the card data as JSON response to the client
+    } catch (error) {
+      console.error('Error reading card data:', error)
+      res.status(500).send('Internal Server Error')
+    }
+  })
 export const cardRoute = Router
